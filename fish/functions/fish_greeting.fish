4da80c716589f5639b554c200144766e19bb345b
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2026-04-18 03:28:14 -0400

    Updated configs

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,10 +1,9 @@
 function fish_greeting
     echo -ne '\x1b[38;5;16m'  # Set colour to primary
-    echo '     ______           __          __  _       '
-    echo '    / ____/___ ____  / /__  _____/ /_(_)___ _ '
-    echo '   / /   / __ `/ _ \/ / _ \/ ___/ __/ / __ `/ '
-    echo '  / /___/ /_/ /  __/ /  __(__  ) /_/ / /_/ /  '
-    echo '  \____/\__,_/\___/_/\___/____/\__/_/\__,_/   '
+    echo '         ___  __  _____  _____  __     '
+    echo '        / _ \/ / / / _ )/ __/ |/ /     '
+    echo '       / , _/ /_/ / _  / _//    /      '
+    echo '      /_/|_|\____/____/___/_/|_/       '
     set_color normal
     fastfetch --key-padding-left 5
 end
